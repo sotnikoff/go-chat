8ab22ab220a79c69bf316f49dd06bacde9c07663
Mikhail Sotnikov <thehofch@gmail.com>
2020-12-18 02:23:07 +0300

    add env support

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/sotnikoff/go-chat
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/kelseyhightower/envconfig v1.4.0
+)
--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/kelseyhightower/envconfig"
 	"log"
 )
 
+// Specification ...
+type Specification struct {
+	Port int
+}
+
 func main() {
+	var s Specification
+	err := envconfig.Process("chat", &s)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	if s.Port == 0 {
+		log.Fatal("Please set the CHAT_PORT env variable!")
+	}
+
 	r := gin.Default()
 	handleRoutes(r)
 
-	err := r.Run()
+	err = r.Run(fmt.Sprint(":", s.Port))
 	if err != nil {
 		log.Fatal(err)
 	}
