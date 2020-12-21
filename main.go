9ed403dc49ebcbdbfbc02d62c639222599f940d0
Mikhail Sotnikov <thehofch@gmail.com>
2020-12-21 12:02:25 +0300

    add shutdown

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/kelseyhightower/envconfig"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 // Specification ...
@@ -20,12 +26,32 @@ func main() {
 	}
 
 	r := gin.Default()
+
 	handleRoutes(r)
 
-	err = r.Run(fmt.Sprint(":", s.Port))
-	if err != nil {
-		log.Fatal(err)
+	srv := &http.Server{
+		Addr:    fmt.Sprint(":", s.Port),
+		Handler: r,
 	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 }
 
 func handleRoutes(r *gin.Engine) {
